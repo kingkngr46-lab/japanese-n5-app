8a040ba26162e2fd5f94d19e25ea75e9f272a34c
kingkngr46-lab <king.kngr46@gmail.com>
2026-01-31 23:52:32 +0530

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,35 @@
+const CACHE_NAME = "jp-n5-cache-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
+
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open("jp-n5-cache").then(cache => {
-            return cache.addAll([
-                "index.html",
-                "manifest.json"
-            ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
-    );
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
-    );
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
